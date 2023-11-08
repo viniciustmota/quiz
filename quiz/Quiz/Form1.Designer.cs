ef9df347804193603ffafb7c5e9e016ec753fe1f
Vinícius Tavares Mota <106537834+viniciustmota@users.noreply.github.com>
2023-11-08 15:31:02 -0300

    Add files via upload

---
+++ quiz/Quiz/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.COMEÇAR = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Quiz.Properties.Resources.titulo;
+            this.pictureBox2.Location = new System.Drawing.Point(91, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(174, 94);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Quiz.Properties.Resources.personagem1;
+            this.pictureBox1.Location = new System.Drawing.Point(337, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 463);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // COMEÇAR
+            // 
+            this.COMEÇAR.BackColor = System.Drawing.Color.White;
+            this.COMEÇAR.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.COMEÇAR.FlatAppearance.BorderSize = 0;
+            this.COMEÇAR.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.COMEÇAR.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.COMEÇAR.Font = new System.Drawing.Font("Bahnschrift Condensed", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.COMEÇAR.Location = new System.Drawing.Point(91, 209);
+            this.COMEÇAR.Name = "COMEÇAR";
+            this.COMEÇAR.Size = new System.Drawing.Size(189, 70);
+            this.COMEÇAR.TabIndex = 2;
+            this.COMEÇAR.Text = "COMEÇAR";
+            this.COMEÇAR.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.COMEÇAR.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(693, 464);
+            this.Controls.Add(this.COMEÇAR);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "d";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button COMEÇAR;
+    }
+}
+
